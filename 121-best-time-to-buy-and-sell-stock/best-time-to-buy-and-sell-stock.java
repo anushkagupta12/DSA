d18cc85f5ef9e8893c589338bda5289badbae3fe
Anushka Gupta <142257330+anushkagupta12@users.noreply.github.com>
2025-10-06 11:42:34 +0530

    Time: 1 ms (99.82%) | Memory: 61.9 MB (31.11%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -14,12 +14,20 @@ class Solution {
         // }
         // return maxProfit;
 
-        int maxPro = 0;
-        int minPrice = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            minPrice = Math.min(minPrice, prices[i]);
-            maxPro = Math.max(maxPro, prices[i] - minPrice);
-        }
-        return maxPro;
+        //  int maxPro = 0;
+        // int minPrice = Integer.MAX_VALUE;
+        // for (int i = 0; i < n; i++) {
+        //     minPrice = Math.min(minPrice, prices[i]);
+        //     maxPro = Math.max(maxPro, prices[i] - minPrice);
+        // }
+        // return maxPro;
+
+       int maxPro = 0;
+       int minPrice = Integer.MAX_VALUE;
+       for(int i = 0; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+        maxPro = Math.max(maxPro, prices[i] - minPrice);
+       }
+       return maxPro;
     }
 }
